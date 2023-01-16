7afb7542b19237c5da33a87dfad0400604a16230
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-01-16 20:44:19 +0800

    Create 哈希表测试.cpp

---
+++ 哈希表测试.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "HashTable.h"
+
+using namespace std;
+
+int main()
+{
+	// CloseHash::TestHashTable1();
+	LinkHash::TestHashTable();
+	return 0;
+}
